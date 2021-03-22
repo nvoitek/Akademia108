a5f812ba5dca3c3e5943835f3af6dc4b25a21e85
nvoitek <nvoitek@gmail.com>
2021-03-22 22:17:15 +0100

    Fix task 7

--- 4-JS/7-obiekty-ksiazka/script.js
+++ 4-JS/7-obiekty-ksiazka/script.js
@@ -1,30 +1,32 @@
 class Book {  
-  constructor(title, author, hasBeenRead) {
-      this.title = title;
-      this.author = author;
-      this.hasBeenRead = hasBeenRead;
-  }
-  describeBook() {
-      let hasBeenReadMsg = this.hasBeenRead ? 'has been read' : 'has not been read';
-      console.log( `Book has title: ${this.title}, author is ${this.author} and it ${hasBeenReadMsg}`);
-  }
+    constructor(title, author, hasBeenRead) {
+        this.title = title;
+        this.author = author;
+        this.hasBeenRead = hasBeenRead;
+    }
+    describeBook() {
+        let hasBeenReadMsg = this.hasBeenRead ? 'has been read' : 'has not been read';
+        console.log( `Book has title: ${this.title}, author is ${this.author} and it ${hasBeenReadMsg}`);
+    }
 }
 
 function countBooksThatHaveBeenRead(books) {
-let booksThatHaveBeenRead = 0;
-for (let book of books) {
-  if (book.hasBeenRead) {
-    booksThatHaveBeenRead++;
+  let booksThatHaveBeenRead = 0;
+  for (let book of books) {
+    if (book.hasBeenRead) {
+      booksThatHaveBeenRead++;
+    }
+
+    book.describeBook()
   }
 
-  book.describeBook()
-}
+  return booksThatHaveBeenRead;
 }
 
 var books = [
-new Book('Wiedzmin', 'Sapkowski', false),
-new Book('Wladca Pierscieni', 'Tolkien', false),
-new Book('Pozytonowy Detektyw', 'Asimov', true),
+  new Book('Wiedzmin', 'Sapkowski', false),
+  new Book('Wladca Pierscieni', 'Tolkien', false),
+  new Book('Pozytonowy Detektyw', 'Asimov', true),
 ]
 
-countBooksThatHaveBeenRead(books);
+console.log(countBooksThatHaveBeenRead(books));
