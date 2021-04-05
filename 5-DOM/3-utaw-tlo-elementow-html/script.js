ccc926f2c819b893c0d846a3400770513c5d5dbe
nvoitek <nvoitek@gmail.com>
2021-04-05 13:34:12 +0200

    Add task 3

---
+++ 5-DOM/3-utaw-tlo-elementow-html/script.js
@@ -0,0 +1,6 @@
+function setBackground() {
+  let paragrahps = document.getElementsByTagName("p");
+
+  paragrahps[0].style.backgroundColor = "red";
+  paragrahps[1].style.backgroundColor = "yellow";
+}
