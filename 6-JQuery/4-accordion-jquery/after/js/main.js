ae598dcd028587e7e41ac4b924865167b773ad5f
nvoitek <nvoitek@gmail.com>
2021-04-25 13:29:45 +0200

    Add task 5

--- 6-JQuery/4-accordion-jquery/after/js/main.js
+++ 6-JQuery/4-accordion-jquery/after/js/main.js
@@ -2,13 +2,11 @@ $(document).ready(function(){
 
     $(".accordion-item").click(function(){
         if ($(this).hasClass("is-open")) {
-            $(this).removeClass("is-open");
             $(this).find(".accordion-content").slideUp();
         } else {
-            $(this).addClass("is-open");
             $(this).find(".accordion-content").slideDown();
         }
-
+        $(this).toggleClass("is-open");
     });
 
 });
