537d409e8e997d7b35dbaffe1f6d7df07fb21455
nvoitek <nvoitek@gmail.com>
2021-05-30 20:27:55 +0200

    add states to login and signup

--- 9-REACT/social-app/src/Login.js
+++ 9-REACT/social-app/src/Login.js
@@ -1,10 +1,33 @@
 import './Login.css';
+import {useState} from 'react';
 
 function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const onPasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const onLogin = (e) => {
+        if (!username) {
+            e.preventDefault();
+        }
+
+        if (!password) {
+            e.preventDefault();
+        }
+    };
+
     return (
-        <form className="Login-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+        <form className="Login-form" onSubmit={onLogin}>
+            <input type="text" placeholder="username" value={username} onChange={onUsernameChange}/>
+            <input type="password" placeholder="password" value={password} onChange={onPasswordChange}/>
             <input type="submit" value="Submit"/>
         </form>
     );
--- 9-REACT/social-app/src/Signup.js
+++ 9-REACT/social-app/src/Signup.js
@@ -1,12 +1,53 @@
 import './Signup.css';
+import {useState} from 'react';
 
 function Signup() {
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const onUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const onEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const onPasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const onConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+
+    const onSignup = (e) => {
+        if (!username) {
+            e.preventDefault();
+        }
+
+        if (!email) {
+            e.preventDefault();
+        }
+
+        if (!password) {
+            e.preventDefault();
+        }
+
+        if (!confirmPassword) {
+            e.preventDefault();
+        }
+    };
+
     return (
-        <form className="Signup-form">
-            <input type="text" placeholder="username"/>
-            <input type="email" placeholder="email"/>
-            <input type="password" placeholder="password"/>
-            <input type="password" placeholder="confirm password"/>
+        <form className="Signup-form" onSubmit={onSignup}>
+            <input type="text" placeholder="username" value={username} onChange={onUsernameChange}/>
+            <input type="email" placeholder="email" value={email} onChange={onEmailChange}/>
+            <input type="password" placeholder="password" value={password} onChange={onPasswordChange}/>
+            <input type="password" placeholder="confirm password" value={confirmPassword} onChange={onConfirmPasswordChange}/>
             <input type="submit" value="Submit"/>
         </form>
     );
